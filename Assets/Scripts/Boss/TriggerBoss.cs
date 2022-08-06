77e65be7a45667d89c206a50f359988d3c2b36b1
pedropifre <pedropifre@gmail.com>
2022-08-06 02:22:03 -0300

    Exercicio32_final

--- Assets/Scripts/Boss/TriggerBoss.cs
+++ Assets/Scripts/Boss/TriggerBoss.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pedro.StateMachine;
+using DG.Tweening;
 
 namespace Boss
 {
@@ -9,10 +10,29 @@ namespace Boss
     public class TriggerBoss : MonoBehaviour
     {
         public BossBase boss;
+        public GameObject bossPrefab;
+        public GameObject bossMesh;
+        public GameObject bossFirstLocation;
+        public Vector3 bossSize;
+        public bool spawned = false;
         private void OnTriggerEnter(Collider other)
         {
-            if(other.transform.tag=="Player")
+            if (other.transform.tag == "Player")
+            {
+                
+                StartCoroutine(SpawnBoss());
                 boss.SwitchState(BossAction.WALK);
+            }
+        }
+        IEnumerator SpawnBoss()
+        {
+            if (!spawned)
+            {             
+                bossPrefab.transform.DOMove(bossFirstLocation.transform.position, 4f).SetEase(Ease.OutBack);
+                spawned = true;
+                yield return new WaitForSeconds(4f);
+            }
+
         }
 
         private void OnTriggerExit(Collider other)
